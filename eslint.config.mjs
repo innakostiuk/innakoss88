251a285947c5eb421863b1a8e700bbb389d73e73
Inna Kostiuk <kostiuk.inna@gmail.com>
2026-06-05 13:02:44 +0800

    init base project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
@@ -13,6 +20,7 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  prettier, // last — disables any ESLint rules that conflict with Prettier
 ]);
 
 export default eslintConfig;
